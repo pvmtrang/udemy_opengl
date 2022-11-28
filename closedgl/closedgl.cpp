d8bc831324a6fba21be4adfc07f824c989d93589
Minh-Trang Pham <minhtrangvietpham@gmail.com>
2022-11-28 18:58:15 +0700

    using uniform var to move triangle

--- closedgl/closedgl.cpp
+++ closedgl/closedgl.cpp
@@ -4,11 +4,17 @@
 #include <iostream>
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+#include <cstdlib>
 
 // Glint, etc is consistent on any compiler != normal int
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformXMove;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.0005f;
 
 //normally shader codes are in separate files
 
@@ -17,10 +23,12 @@ static const char* vShader = "                                                \n
 #version 330                                                                  \n\
                                                                               \n\
 layout (location = 0) in vec3 pos;											  \n\
+										  \n\
+uniform float xMove		;									  \n\
                                                                               \n\
 void main()                                                                   \n\
 {                                                                             \n\
-    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);				                \n\
+    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y+ xMove, pos.z, 1.0);				  \n\
 }";
 
 // Fragment Shader
@@ -131,6 +139,7 @@ void CompileShaders()
         return;
     }
 
+	uniformXMove = glGetUniformLocation(shader, "xMove");
 }
 
 
@@ -190,10 +199,26 @@ int main()
 
     while (!glfwWindowShouldClose(mainWindow)) {
         glfwPollEvents();
+
+        if (direction)
+        {
+            triOffset += triIncrement;
+        }
+        else {
+            triOffset -= triIncrement;
+        }
+
+        if (abs(triOffset) >= triMaxOffset)
+        {
+            direction = !direction;
+        }
+
         glClearColor(0.3f, 0.2f, 0.8f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
+
+        glUniform1f(uniformXMove, triOffset);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
